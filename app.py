cd037ab5e7d89d6c232f3a1bf96a7d62b9270f88
haruzong1 <41902380+haruzong1@users.noreply.github.com>
2020-08-10 21:39:01 +0900

    메모리스트 api

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def post_article():
     title = og_title ['content']# 태그의 속성값을 가져오기
     og_image = soup.select_one('meta[property="og:image"]')
     image = og_image['content']
-    og_description = soup.select_one('meta[property="og:description]')
+    og_description = soup.select_one('meta[property="og:description"]')
     description = og_description['content']
 
     # 3. mongoDB에 데이터 넣기
@@ -46,8 +46,15 @@ def post_article():
 @app.route('/memo', methods=['GET'])
 def read_articles():
     # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
+    articles = list(db.alonememo.find({},{'_id':0}))
     # 2. articles라는 키 값으로 articles 정보 보내주기
-    return jsonify({'result': 'success', 'msg': 'GET 연결되었습니다!'})
+    result ={
+        'result': 'success',
+        'articles':articles,
+    }
+
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
